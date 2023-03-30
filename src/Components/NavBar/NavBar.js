484758b8cbac2d902d09e733aeb71ce719c542e5
James Linkous <james.william.linkous@gmail.com>
2023-03-29 20:11:48 -0500

    practice

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,7 +4,8 @@ import "./NavBar.css"
 export const NavBar = () => {
     return (
         <ul className="navbar">
-            <li><Link to="useRef">useRef</Link></li>
+            <li className="navbar-item"><Link to="useRef">useRef</Link></li>
+            <li className="navbar-item"><Link to="useReducer">useReducer</Link></li>
         </ul>
     )
 }
